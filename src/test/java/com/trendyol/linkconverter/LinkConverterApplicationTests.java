d4dbba749ae4453e1b98039a8e141b297a651987
Oleksii_Mutianov <omut@ciklum.com>
2022-03-22 15:48:01 +0200

    Initial commit

---
+++ src/test/java/com/trendyol/linkconverter/LinkConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendyol.linkconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
